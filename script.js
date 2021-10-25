a26177da9a84780af26bb512a2cecd9d7e349828
Tim Davidson <timdavidson2@gmail.com>
2021-10-24 23:15:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const timeEl = document.getElementById("time");
 const dateEl = document.getElementById("date");
 const currentWeatherItemsEl = document.getElementById("current-weather-items");
-const timeZoneEl = document.getElementById(time - zone);
+const timeZoneEl = document.getElementById("time-zone");
+const countryEl = document.getElementById("country");
